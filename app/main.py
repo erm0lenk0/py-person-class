1c6060705f6461e6a33522be26066319e9bf2be3
Ermolenko Andrey <f1nd6r@gmail.com>
2025-04-07 10:39:49 +0300

    bug_fix2

--- app/main.py
+++ app/main.py
@@ -9,28 +9,26 @@ class Person:
 
         Person.people[name] = self
 
+    def create_person_list(people_list: list) -> list:
+        person_instances = []
+        for person_dict in people_list:
+            name = person_dict["name"]
+            age = person_dict["age"]
 
-def create_person_list(people_list: list) -> list:
-    person_instances = []
+            person_instance = Person(name, age)
 
-    for person_dict in people_list:
-        name = person_dict["name"]
-        age = person_dict["age"]
+            person_instances.append(person_instance)
 
-        person_instance = Person(name, age)
-        person_instances.append(person_instance)
+        for person_dict in people_list:
+            name = person_dict["name"]
+            wife_name = person_dict.get("wife")
+            if wife_name is not None and wife_name in Person.people:
+                Person.people[name].wife = Person.people[wife_name]
+                Person.people[wife_name].husband = Person.people[name]
 
-    for person_dict in people_list:
-        name = person_dict["name"]
+            husband_name = person_dict.get("husband")
+            if husband_name is not None and wife_name in Person.people:
+                Person.people[name].husband = Person.people[husband_name]
+                Person.people[husband_name].wife = Person.people[name]
 
-        wife_name = person_dict.get("wife")
-        if wife_name is not None and wife_name in Person.people:
-            Person.people[name].wife = Person.people[wife_name]
-            Person.people[wife_name].husband = Person.people[name]
-
-        husband_name = person_dict.get("husband")
-        if husband_name is not None and wife_name in Person.people:
-            Person.people[name].husband = Person.people[husband_name]
-            Person.people[husband_name].wife = Person.people[name]
-
-    return person_instances
+        return person_instances
