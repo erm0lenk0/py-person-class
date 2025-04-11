039fa4d5a565e0ea99fbad978f3f7305f24c7a6b
Ermolenko Andrey <f1nd6r@gmail.com>
2025-04-11 11:31:18 +0300

    final decision

--- app/main.py
+++ app/main.py
@@ -4,31 +4,30 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.wife = None
-        self.husband = None
-
         Person.people[name] = self
 
-    def create_person_list(people_list: list) -> list:
-        person_instances = []
-        for person_dict in people_list:
-            name = person_dict["name"]
-            age = person_dict["age"]
 
-            person_instance = Person(name, age)
+def create_person_list(people: list) -> list:
+    person_instances = []
 
-            person_instances.append(person_instance)
+    for person_dict in people:
+        name = person_dict["name"]
+        age = person_dict["age"]
+        person_instance = Person(name, age)
+        person_instances.append(person_instance)
 
-        for person_dict in people_list:
-            name = person_dict["name"]
-            wife_name = person_dict.get("wife")
-            if wife_name is not None and wife_name in Person.people:
-                Person.people[name].wife = Person.people[wife_name]
-                Person.people[wife_name].husband = Person.people[name]
+    for person_dict in people:
+        name = person_dict["name"]
+        person_instance = Person.people[name]
 
-            husband_name = person_dict.get("husband")
-            if husband_name is not None and wife_name in Person.people:
-                Person.people[name].husband = Person.people[husband_name]
-                Person.people[husband_name].wife = Person.people[name]
+        wife_name = person_dict.get("wife")
+        if wife_name is not None:
+            wife_instance = Person.people[wife_name]
+            person_instance.wife = wife_instance
 
-        return person_instances
+        husband_name = person_dict.get("husband")
+        if husband_name is not None:
+            husband_instance = Person.people[husband_name]
+            person_instance.husband = husband_instance
+
+    return person_instances
